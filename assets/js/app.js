6519759e0bb2be9d7091bd707d4e5e0e46c895e4
m4th142 <136912799+m4th142@users.noreply.github.com>
2025-10-21 08:03:44 -0300

    deck and takerCart added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,35 @@
-console.log('hello!');
+
+
+const crearDeck = () => {
+
+    let deck = [];
+    let kinds = ['C','D', 'S', 'H'];
+    let specials = ['A', 'J', 'K', 'Q'];
+
+    for( let i = 2; i <= 10; i++ ){
+        for( kind of kinds ){
+            deck.push( i + kind );
+        }
+    }
+
+    for( special of specials ){
+        for( kind of kinds ){
+            deck.push( special + kind );
+        }
+    }
+
+    return deck = _.shuffle( deck );
+
+}
+
+let deck = crearDeck(); 
+
+
+const tomarCarta = () => {
+    if( deck.length === 0 ){
+        throw 'No hay cartas';
+    }
+
+    return deck.shift();
+}
+
